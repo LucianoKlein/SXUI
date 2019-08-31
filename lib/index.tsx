91e6ba870b4840331d4b87ff3cbd8b3de2c8d821
kleinersilver <kleinersilver@gmail.com>
2019-08-31 14:08:23 -0700

    install react and reactDOM

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,5 @@
-console.log("fuck");
-const div = document.createElement('div');
-div.innerText='fuck';
-document.body.appendChild(div);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log(React);
+console.log(ReactDOM);
