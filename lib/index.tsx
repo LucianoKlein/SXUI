a0a3b6eeb45dd8ac1738a80198bc128b13643b62
kleinersilver <kleinersilver@gmail.com>
2019-09-04 18:12:27 -0700

    git add icon & modify index.tsx

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './button';
+import React from 'react';
+import Icon from './icon';
 
-ReactDOM.render(<Button></Button>, document.body);
-
-export {Button};
+ReactDOM.render(<div><Icon name={"wechat"}/></div>, document.body);
