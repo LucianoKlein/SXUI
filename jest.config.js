55c44f27a50fa8d9e55ea151252c03b6133380ee
kleinersilver <kleinersilver@gmail.com>
2019-10-03 15:41:18 -0700

    add ci config

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,7 @@
 module.exports = {
     verbose: true,
     clearMocks: false,
-    collectCoverage: true,
-    collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
-    coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov'],
+    collectCoverage: false,
     reporters: ["default", "jest-junit"],
     // globals: {
     //     'ts-jest': {
@@ -15,6 +12,11 @@ module.exports = {
     // },
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     moduleDirectories: ['node_modules'],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.json',
+        },
+    },
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|map3|m4a|aac|oga)$":"<rootDir>/test/__mocks__/file-mock.js",
         "\\.(css|less|sass|scss)$":"<rootDir>/test/__mocks__/object-mock.js"
