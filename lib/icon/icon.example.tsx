fcc35a9c956b66ff7215e28207f0b09f4365295c
kleinersilver <kleinersilver@gmail.com>
2019-10-03 16:55:37 -0700

    add examples page and two demo pages

---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Icon from './icon';
+
+const IconExample:React.FunctionComponent = () => {
+    return (
+        <div>
+            <Icon name="paypal"/>
+            <Icon name="wechat"/>
+            <Icon name="Facebook"/>
+        </div>
+    )
+};
+
+export default IconExample;
